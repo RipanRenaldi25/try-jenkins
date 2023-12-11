cd2149b09b3ab8658ceff4d5360cc2210961afa0
RipanRenaldi25 <ripanrenaldi25042002@gmail.com>
2023-12-11 13:06:07 +0700

    test

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 const router = express_1.default.Router();
 router.get('/', (req, res) => {
-    res.send('oke');
+    res.send('oke2');
 });
 app.use('/', router);
-app.listen(3000, () => console.log('Running on port 3000'));
+app.listen(+process.env.PORT_APP, () => console.log(`Running on port ${process.env.PORT_APP}`));
